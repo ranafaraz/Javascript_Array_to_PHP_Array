f370633f5831d4784d01e713517727a059082cb2
Aqsa099 <aqsachoudhary402@gmail.com>
2017-08-24 12:22:08 +0500

    php code are written in this file

---
+++ myPhpPage.php
@@ -0,0 +1,27 @@
+<?php
+$secret = $_POST['one'];
+
+$ar=explode(',',$secret);
+print_r($ar);
+var_dump($ar);
+        echo "<hr>";
+
+
+$show = $_POST['two'];
+$arr=explode(',',$show);
+print_r($arr);
+var_dump($arr);
+        echo "<hr>";
+
+$show = $_POST['three'];
+$arr=explode(',',$show);
+print_r($arr);
+var_dump($arr);
+        echo "<hr>";
+		
+$show = $_POST['four'];
+$arr=explode(',',$show);
+print_r($arr);
+var_dump($arr);
+        echo "<hr>";
+?>
